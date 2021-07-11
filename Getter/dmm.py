5e951223fd20135e6855b40296b663828ff04125
Hermit10 <gaozhh@qq.com>
2021-07-12 00:37:57 +0800

    Add files via upload

--- Getter/dmm.py
+++ Getter/dmm.py
@@ -4,12 +4,20 @@ import re
 from lxml import etree
 import json
 from Function.getHtml import get_html, get_cookies
+import urllib3
+urllib3.disable_warnings()
+
 
 def getTitle(html):
-    result = html.xpath('//img[@class="tdmm"]/@alt')
-    if result:
-        return result[0]
-    return ''
+    result1 = html.xpath('//h1[@id="title"]/text()')
+    result2 = html.xpath('//h1[@class="item fn bold"]/text()')
+    a = ''
+    b = ''
+    if result1:
+        a = result1[0].strip('"')
+    if result2:
+        b = result2[0].strip('"')
+    return a + b
 
 
 def getActor(html): 
@@ -55,10 +63,10 @@ def getNum(html):
 
 def getYear(release):
     try:
-        result = str(re.search('\d{4}', getRelease).group())
+        result = str(re.search('\d{4}', release).group())
         return result
     except:
-        return release
+        return release[:4]
 
 
 def getRelease(html):
@@ -75,9 +83,15 @@ def getTag(html):
     return ''
 
 
-
 def getCover(html):
     result = html.xpath('//a[@name="package-image"]/@href')
+    if result:
+        return result[0]
+    return ''
+
+
+def getCoverSmall(html):
+    result = html.xpath('//img[@class="tdmm"]/@src')
     if result:
         return result[0]
     return ''
@@ -98,10 +112,15 @@ def getDirector(html):
 
 
 def getOutline(html):
-    result = html.xpath("//p[@class='mg-b20']/text()")
-    if result:
-        return result[0].replace('\\n', '').replace('\n', '')
-    return ''
+    result1 = html.xpath("//div[@class='mg-b20 lh4']/text()")
+    result2 = html.xpath("//div[@class='mg-b20 lh4']/p[@class='mg-b20']/text()")
+    a = ''
+    b = ''
+    if result1:
+        a =  result1[0].replace('\\n', '').replace('\n', '').strip()
+    if result2:
+        b =  result2[0].replace('\\n', '').replace('\n', '').strip()
+    return a + b
 
 
 def getScore(html):
@@ -112,7 +131,7 @@ def getScore(html):
 
 def main(number, appoint_url='', log_info=''):
     cookies = get_cookies('dmm')
-    log_info += '   >>> DMM-开始使用 dmm 进行刮削\n'
+    log_info += '   >>> [ DMM ] 开始使用 dmm 进行刮削\n'
     title = ''
     cover_url = ''
     cover_small = ''
@@ -123,74 +142,87 @@ def main(number, appoint_url='', log_info=''):
     url = 'https://www.dmm.co.jp/search/=/searchstr=' + new_number
     num1 = '/cid=' + new_number + '/'
     num2 = new_number + '/'
+
+    if appoint_url: # 如果传入地址，则使用传入的地址
+        url = appoint_url
     try:
-        if appoint_url: # 如果传入地址，则使用传入的地址
-            url = appoint_url
         result, htmlcode = get_html(url, cookies=cookies)
-        if not result:
-            log_info += '   >>> DMM-请求URL：%s 出现错误：%s' % (url, htmlcode)
-            error_type = 'timeout'
-            raise Exception('DMM-请求URL：%s 出现错误：%s' % (url, htmlcode))
+        # 对各种错误、限制进行判断
+        if not result:  # 请求失败
+            error_type = 'request error'  # error_type 只是标记失败，内容暂不展示，可随便写
+            error_info = '[ DMM ] 请求URL：%s 出现错误：%s' % (url, htmlcode)   # error_info 在主界面标题位置展示
+            log_info += '   >>> [ DMM ] 请求URL：%s 出现错误：%s\n' % (url, htmlcode) # log_info 在日志页面展示
+            raise
+        if re.findall('foreignError', htmlcode):    # 非日本地区限制访问
+            error_type = 'area error'
+            error_info = '[ DMM ] 地域限制, 请使用日本节点访问！'
+            log_info += '   >>> [ DMM ] 地域限制, 请使用日本节点访问！\n'
+            raise
+        html = etree.fromstring(htmlcode, etree.HTMLParser())
 
-        if re.findall('foreignError', htmlcode):
-            error_info = '   >>> DMM-地域限制, 请使用日本节点访问！'
-            log_info += '   >>> DMM-地域限制, 请使用日本节点访问！'
-            error_type = 'area blocked'
-            raise Exception('DMM-地域限制, 请使用日本节点访问！')
-
-        if re.findall('ageCheck', htmlcode):   # 如果页面有ageCheck，表示无cookie或cookie失效，需要进行年龄认证
-            if cookies:
-                log_info += '   >>> DMM-Cookie 已失效，请到设置中更新 Cookie！\n'
-            else:
-                log_info += '   >>> DMM-请到【设置】-【网络设置】中添加 dmm Cookie！\n'
-            error_type = 'need cookie'
-            raise Exception('DMM-年龄认证拦截，需更新Cookie！')
-
-        html = etree.fromstring(htmlcode, etree.HTMLParser())   # 当前如果是传入的url就是详情页，如果不是传入url，就是搜索页
-        if appoint_url:  # 如果是传入的地址，判断页面地址是否正确
-            if '404 Not Found' in str(html.xpath("//span[@class='d-txten']/text()")):   # 如果页面有404，表示传入的页面地址不对
-                log_info += '   >>> DMM-未匹配到番号' 
-                error_type = 'Movie data not found'
-                raise Exception('DMM-未匹配到番号')
-        else:            # 如果不是传入的地址，则上面请求的是搜索页，需要再解析搜索页，获取详情页地址
+        # 解析搜索页，如果是传入url，则跳过此环节
+        if not appoint_url:
+            # 匹配详情页地址
             if html.xpath("//a[contains(@href, $val)]", val=num1): # 优先匹配'/cid=snis126/'这样链接的，图上面没有蓝光水印
                 url = html.xpath("//a[contains(@href, $val)]/@href", val=num1)[0]
             elif html.xpath("//a[contains(@href, 'detail')][contains(@href, $val)]/@href", val=num2): # 如果链接中包含detail和number，则表示找到了
                 url = html.xpath("//a[contains(@href, 'detail')][contains(@href, $val)]/@href", val=num2)[0]
             else:
-                log_info += '   >>> DMM-未匹配到番号' 
-                error_type = 'Movie data not found'
-                raise Exception('DMM-未匹配到番号')                    
-            result, htmlcode = get_html(url, cookies=cookies)   # 获取详情页内容
+                error_type = 'not found the movie'
+                error_info = '[ DMM ] 搜索页未匹配到番号！'
+                log_info += '   >>> [ DMM ] 搜索页未匹配到番号！\n' 
+                raise            # 请求详情页
+            url = url.replace('?i3_ref=search&i3_ord=1', '')
+            result, htmlcode = get_html(url, cookies=cookies)
             html = etree.fromstring(htmlcode, etree.HTMLParser())
+
+        # 分析详情页
+        if re.findall('ageCheck', htmlcode):   # 年龄认证，表示无cookie或cookie失效
+            if cookies['cookie']:
+                error_info = '[ DMM ] 年龄认证！需要重新设置cookie！'
+                log_info += '   >>> [ DMM ] 年龄认证！需要重新设置cookie！\n'
+            else:
+                error_info = '[ DMM ] 年龄认证！请到【设置】-【网络设置】中添加 cookie！'
+                log_info += '   >>> [ DMM ] 年龄认证！请到【设置】-【网络设置】中添加 cookie！\n'
+            raise
+        if '404 Not Found' in str(html.xpath("//span[@class='d-txten']/text()")):   # 如果页面有404，表示传入的页面地址不对
+            error_type = 'detail page url error 404 not found!'
+            error_info = '[ DMM ] 详情页地址不对！404 Not Found!'
+            log_info += '   >>> [ DMM ] 详情页地址不对！404 Not Found！\n' 
+            raise
+
         actor = getActor(html)  # 获取歌手
-        title = getTitle(html).strip(actor) # 获取标题（去掉标题最后的歌手名）
+        title = getTitle(html).strip(actor).strip() # 获取标题（去掉标题最后的歌手名）
         if not title:
-            log_info += '   >>> DMM- title 获取失败！ \n'
-            error_type = 'need login'
-            raise Exception('DMM- title 获取失败！')
+            error_type = 'title data not found'
+            error_info = '[ DMM ]  标题数据未匹配到！'
+            log_info += '   >>> [ DMM ]  标题数据未匹配到！ \n'
+            raise
         cover_url = getCover(html) # 获取cover
         if not cover_url:
-            log_info += '   >>> DMM- cover url 获取失败！ \n'
-            error_type = 'Cover Url is None!'
-            raise Exception('DMM- cover url 获取失败！')
+            error_type = 'cover URL not found'
+            error_info += '[ DMM ]  cover URL 未匹配到！'
+            log_info += '   >>> [ DMM ]  cover URL 未匹配到！ \n'
+            raise
         try:
-            studio = str(getStudio(html))
             outline = getOutline(html)
-            score = getScore(html)
-            runtime = getRuntime(html)
-            director = getDirector(html)
-            release = getRelease(html)
-            # number = getNum(html)
             tag = getTag(html)
-            series = getSeries(html).replace('-', '')
+            release = getRelease(html)
             year = getYear(release)
+            runtime = getRuntime(html)
+            score = getScore(html)
+            series = getSeries(html)
+            director = getDirector(html)
+            # number = getNum(html)
+            studio = getStudio(html)
             extrafanart = getExtraFanart(html)
+            cover_small = getCoverSmall(html)
             actor_photo = getActorPhoto(actor)
         except Exception as error_info:
-                log_info += '   >>> DMM-获取data信息时出错(dmm.py)！ 错误信息：%s\n' % str(error_info)
-                error_info = str(error_info)
-                raise Exception(log_info)            
+            error_type = 'error'
+            error_info = '[ DMM ] 一些 data 在获取时出错！'
+            log_info += '   >>> [ DMM ] 一些 data 在获取时出错！！ %s\n' % str(error_info)
+            raise
         try:
             dic = {
                 'title': title,
@@ -210,25 +242,26 @@ def main(number, appoint_url='', log_info=''):
                 'website': url,
                 'actor_photo': actor_photo,
                 'cover': str(cover_url),
-                'cover_small': '',
+                'cover_small': cover_small,
                 'extrafanart': extrafanart,
                 'imagecut': 1,
                 'log_info': str(log_info),
                 'error_type': '',
                 'error_info': str(error_info),
             }
-            log_info += '   >>> DMM-数据获取成功！\n'
+            log_info += '   >>> [ DMM ] 数据获取成功！\n'
             dic['log_info'] = log_info
         except Exception as error_info:
-                log_info += '   >>> DMM-生成数据字典：出错！ 错误信息：%s\n' % str(error_info)
-                error_info = str(error_info)
-                raise Exception(log_info)
+            error_type = 'dic error'
+            error_info = '[ DMM ] 生成数据字典时出错！'
+            log_info += '   >>> [ DMM ] 生成数据字典时出错！ %s\n' % str(error_info)
+            raise
 
-    except Exception as error_info:
+    except:
         dic = {
             'title': '',
             'cover': '',
-            'website': str(url).strip('[]'),
+            'website': str(url),
             'log_info': str(log_info),
             'error_type': str(error_type),
             'error_info': str(error_info),
@@ -237,12 +270,13 @@ def main(number, appoint_url='', log_info=''):
     return js
 
 
+# print(main('ssni888'))
+# print(main('ssni00999'))
 # print(main('snis-027'))
 # print(main('ipx-292'))
 # print(main('wicp-002'))
 # print(main('ssis-080'))
-# print(main('ssis-080'))
-# main('DV-1562')
+# print(main('DV-1562'))
 # print(main('mide00139', "https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=mide00139"))
 # print(main('mide00139', ""))
 # print(main('kawd00969'))
